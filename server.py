e1aac78b7162c6c9bb1928c3ed4181a104c8ea16
LoginN2 <167524850+LoginN2@users.noreply.github.com>
2024-05-06 16:27:22 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,3 +12,7 @@ print('Сервер запущен на {}:{}'.format(*server_address))
 while True:
     print('Ожидание соединения...')
     connection, client_address = server_socket.accept()
+    data = connection.recv(1024).decode()
+
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={data}&appid={API_KEY}&units=metric"
+    response = requests.get(url).json()
